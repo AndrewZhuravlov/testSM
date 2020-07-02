90ce4ad1dc15b6b86987c71de2d7c21356859b7f
AndrewZhuravlov <63732647+AndrewZhuravlov@users.noreply.github.com>
2020-07-02 17:53:41 +0300

    modul.css

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import s from './header.module.css'
 
 function Header(){
     return(
-    <header className='head'>
+    <header className= {s.head}>
         <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/1000px-Apple_logo_black.svg.png' alt="" />
       </header>
     )
